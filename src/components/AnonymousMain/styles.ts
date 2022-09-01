34526e71d2ee3e3508ba09118eda5c3cfc0d0224
unknown <elizeucosta21@gmail.com>
2022-09-01 20:39:21 -0300

    feat(anonymous page): add component AnonymousMain

---
+++ src/components/AnonymousMain/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const StyledAnonymousMain = styled.div`
+  width: 100%;
+  height: 80vh;
+  padding: 0 2rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CardList = styled.ul`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  gap: 2rem;
+
+  @media (max-width: 750px) {
+    justify-content: center;
+  }
+`;
