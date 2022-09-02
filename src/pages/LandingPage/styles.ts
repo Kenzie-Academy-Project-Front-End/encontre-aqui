0600dac30e73dabded59d691518bbbeb01745d9e
Paola Gomes Nantes <36554079+PaolaNantes@users.noreply.github.com>
2022-09-02 08:07:16 -0400

    feat: landing page

---
+++ src/pages/LandingPage/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const StyledLandingPage = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1.5rem;
+  align-items: center;
+
+  /* .logo {
+    display: flex;
+    width: 250px;
+  }
+
+  .title {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 2rem;
+  } */
+
+  .buttons {
+    display: flex;
+    min-width: 50%;
+    text-align: center;
+    margin-top: 1rem;
+    gap: 1.5rem;
+  }
+`;
