752dd322e9c18d57d94c0b89f4219a2d9bccf96b
leandroschillreff <leandroschillreff@gmail.com>
2022-09-03 17:00:31 -0400

    refactor(styles): user register

--- src/components/Container/index.ts
+++ src/components/Container/index.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  /* flex-direction: column; */
   width: 100vw;
   max-width: 1440px;
   margin: 0 auto;
