7e89cdc3f73e46fe17ecd530557a1f40a10b3d23
leandroschillreff <leandroschillreff@gmail.com>
2022-09-03 17:01:25 -0400

    refactor(routes): rename user register

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import AnonymousNavigate from '../pages/Anonymous';
 import { LandingPage } from '../pages/LandingPage';
-import { RegisterUser } from '../pages/RegisterUser';
+import { UserRegister } from '../pages/UserRegister';
 import { UserLogin } from '../pages/UserLogin';
 
 export const RoutesMain = () => (
   <Routes>
     <Route path='/' element={<LandingPage />} />
-    <Route path='/register' element={<RegisterUser />} />
+    <Route path='/register' element={<UserRegister />} />
     <Route path='/login' element={<UserLogin />} />
     <Route path='/anonymous' element={<AnonymousNavigate />} />
   </Routes>
