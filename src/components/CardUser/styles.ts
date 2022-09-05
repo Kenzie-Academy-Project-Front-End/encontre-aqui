a9897be994eec5296ad93d95c39920d24df035f2
unknown <elizeucosta21@gmail.com>
2022-09-05 14:57:56 -0300

    feat(user page): add tag for item claimed by user

--- src/components/CardUser/styles.ts
+++ src/components/CardUser/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 interface IStyledCardUser {
   user: number;
   userLog: number;
+  claim: number;
 }
 
 export const StyledCardUser = styled.li<IStyledCardUser>`
@@ -16,7 +17,8 @@ export const StyledCardUser = styled.li<IStyledCardUser>`
   position: relative;
   animation: scale-in 1.5s;
   background-color: var(--color-white);
-  opacity: ${({ userLog, user }) => (userLog === user ? 0.5 : 1)};
+  opacity: ${({ userLog, user, claim }) =>
+    userLog === user || user === claim ? 0.5 : 1};
 
   @keyframes scale-in {
     0% {
