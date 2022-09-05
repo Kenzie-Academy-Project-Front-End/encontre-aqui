5d3dc5c2c2388ea7a36b828cc7b1636c226d0745
leandroschillreff <leandroschillreff@gmail.com>
2022-09-05 17:59:03 -0400

    refactor(Container): updating styles

--- src/components/Container/index.ts
+++ src/components/Container/index.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  height: 100%;
   max-width: 1440px;
   margin: 0 auto;
   padding: 1rem;
